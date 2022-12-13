7a4e04a28d437753a8cdf17cbcdb01e35c6079be
Mihail Croitor <mcroitor@gmail.com>
2022-12-13 18:29:36 +0200

    added app for simulation

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main()
 {
-    std::cout << "Thread programm" << std::endl;
+    int timeoutInMs, memorySizeInKb;
+    std::cin >> timeoutInMs >> memorySizeInKb;
+    
+    std::int8_t* allocatedMemory = new std::int8_t[memorySizeInKb * 1024];
+
+    std::cout << "allocated " << memorySizeInKb << "KB successfully." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeoutInMs));
+
+    std::cout << "done after " << timeoutInMs << "ms." << std::endl;
+
+    delete allocatedMemory;
     return 0;
 }
