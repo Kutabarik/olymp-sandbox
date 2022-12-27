6704ef58a1c3ec04d3b5d17a6d8b0db959622b4f
Mihail Croitor <mcroitor@gmail.com>
2022-12-27 10:04:28 +0200

    test program fixed

--- src/test.cpp
+++ src/test.cpp
@@ -9,12 +9,13 @@ int main()
     int timeoutInMs, memorySizeInKb;
     std::cin >> timeoutInMs >> memorySizeInKb;
 
-    std::int8_t *allocatedMemory = new std::int8_t[memorySizeInKb * 1024];
+    std::int8_t *allocatedMemory = new std::int8_t[memorySizeInKb * 1024]{};
 
-    std::cout << "allocated " << memorySizeInKb << "KB successfully." << std::endl;
+    std::cout << "allocated " << memorySizeInKb << " KB successfully." << std::endl;
+    
     std::this_thread::sleep_for(std::chrono::milliseconds(timeoutInMs));
 
-    std::cout << "done after " << timeoutInMs << "ms." << std::endl;
+    std::cout << "done after " << timeoutInMs << " ms." << std::endl;
 
     delete allocatedMemory;
     return 0;
